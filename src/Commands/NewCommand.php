648afc73613166d8a4bd5fecb1a570dd01921377
Denilson Silva <dbsdenis@hotmail.com>
2026-04-02 09:23:01 -0300

    feat: add --no-docs option to new command

--- src/Commands/NewCommand.php
+++ src/Commands/NewCommand.php
@@ -5,6 +5,7 @@ namespace SparkPhp\Installer\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -15,13 +16,19 @@ class NewCommand extends Command
         $this
             ->setName('new')
             ->setDescription('Cria um novo projeto SparkPHP')
-            ->addArgument('name', InputArgument::REQUIRED, 'Nome do projeto');
+            ->addArgument('name', InputArgument::REQUIRED, 'Nome do projeto')
+            ->addOption('no-docs', null, InputOption::VALUE_NONE, 'Cria o projeto sem a documentação');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io   = new SymfonyStyle($input, $output);
-        $name = $input->getArgument('name');
+        $io     = new SymfonyStyle($input, $output);
+        $name   = $input->getArgument('name');
+        $noDocs = $input->getOption('no-docs');
+
+        if (!$noDocs && $input->isInteractive()) {
+            $noDocs = !$io->confirm('Incluir documentação no projeto?', true);
+        }
 
         $io->title('SparkPHP Installer');
         $io->text("Criando projeto <info>{$name}</info>...");
@@ -49,6 +56,10 @@ class NewCommand extends Command
             return Command::FAILURE;
         }
 
+        if ($noDocs) {
+            $this->removeDir($name . '/docs');
+        }
+
         $io->newLine();
         $io->success("Projeto '{$name}' criado com sucesso!");
         $io->listing([
@@ -57,6 +68,24 @@ class NewCommand extends Command
         ]);
 
         return Command::SUCCESS;
+    }
+
+    private function removeDir(string $path): void
+    {
+        if (!is_dir($path)) {
+            return;
+        }
+
+        $items = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($items as $item) {
+            $item->isDir() ? rmdir($item->getRealPath()) : unlink($item->getRealPath());
+        }
+
+        rmdir($path);
     }
 
     private function commandExists(string $command): bool
