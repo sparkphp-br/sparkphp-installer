36e2f75be844e22b20e053543a929b1115552a13
Denilson Silva <dbsdenis@hotmail.com>
2026-04-02 12:01:23 -0300

    chore: bump version to 1.1.4

--- src/Application.php
+++ src/Application.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Application as SymfonyApplication;
 
 class Application
 {
-    public const VERSION = '1.1.3';
+    public const VERSION = '1.1.4';
 
     public function run(): void
     {
